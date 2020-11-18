da502105e122a868e6a5057c45aaff39ee17a3c2
Karla Pérez <44835870+karlaprzbr@users.noreply.github.com>
2020-11-18 12:45:21 +0100

    Update morning exercises

--- 2020-11-18-matin-house-pricing.py
+++ 2020-11-18-matin-house-pricing.py
@@ -1,11 +1,36 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import altair as alt
 
 st.title('House pricing')
 
 DATE_COLUMN = 'date/time'
-data = pd.read_csv("house_pricing.csv")
-st.write(data)
-data2 = data.info()
-st.write(data2)
+price = pd.read_csv("house_pricing.csv")
+
+st.subheader('Raw data')
+st.write(price)
+
+st.subheader('Liste des colonnes')
+st.write(price.columns.tolist())
+
+st.subheader('Type des colonnes')
+st.write(price.dtypes)
+
+st.subheader('Shape du dataset')
+c = alt.Chart(price).mark_circle().encode(
+    x='LotArea', y='SalePrice'
+)
+st.write(c)
+
+st.subheader('Statistiques descriptives du dataset')
+st.write(price.describe())
+
+st.subheader('Heatmap')
+fig, ax = plt.subplots(figsize=(10,10))
+sns.heatmap(price.corr(), annot=True, ax=ax)
+st.pyplot(fig)
+#df_price = pd.DataFrame(price)
+#st.dataframe(df_price)
