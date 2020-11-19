40ba32a1758220dd609f45c5946f5175b4b5e9e6
Karla Pérez <44835870+karlaprzbr@users.noreply.github.com>
2020-11-19 12:36:14 +0100

    add route for json file

--- 2020-11-19 app flask/app/views.py
+++ 2020-11-19 app flask/app/views.py
@@ -15,9 +15,17 @@ def hello(name = None):
 @app.route('/books')
 def books():
     with open('F:/GitHub/m1-python/2020-11-19 app flask/app/books.json') as f:
-        data = json.load(f)
-    #return jsonify(data)
-    return render_template('books.html', books=jsonify(data))
+        return render_template('books.html', books=json.load(f))
+
+@app.route('/books/<isbn>')
+def book_by_isbn(isbn = None):
+    with open('F:/GitHub/m1-python/2020-11-19 app flask/app/books.json') as f:
+        return render_template('books.html', books=json.load(f), isbn=isbn)
+
+@app.route('/books/<title>')
+def book_by_title(title = None):
+    with open('F:/GitHub/m1-python/2020-11-19 app flask/app/books.json') as f:
+        return render_template('books.html', books=json.load(f), title=title)
 
 if __name__ == "__main__":
     app.run(debug=True)
